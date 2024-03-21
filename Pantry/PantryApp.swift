c22718ed1960709c8cc6eaecac372bd11eb07f40
Alea <aleaminar@gmail.com>
2024-03-20 19:23:30 -0700

    Initial Commit

---
+++ Pantry/PantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryApp.swift
+//  Pantry
+//
+//  Created by Alea Minar on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
